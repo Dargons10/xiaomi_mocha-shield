bb269a5cf3aaa1c0db2509b85a19577601818078
apple <105837597+380583067@users.noreply.github.com>
2023-06-23 11:05:48 +0000

    Update and rename lineage.mk to arrow.mk

--- arrow.mk
+++ arrow.mk
@@ -2,9 +2,9 @@
 $(call inherit-product, device/xiaomi/mocha/full_mocha.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/lineage/config/common_mini_tablet_wifionly.mk)
+$(call inherit-product, vendor/arrow/config/common.mk)
 
-PRODUCT_NAME := lineage_mocha
+PRODUCT_NAME := arrow_mocha
 PRODUCT_DEVICE := mocha
 BOARD_VENDOR := Xiaomi
 
