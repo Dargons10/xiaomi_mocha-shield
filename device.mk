be09e9e772857d22b93150927d953ceaac2b6ec0
apple <105837597+380583067@users.noreply.github.com>
2023-07-12 06:39:02 +0000

    error: Audio policy no longer supports legacy .conf configuration format
    frameworks/av/services/audiopolicy/managerdefault/AudioPolicyManager.cpp:4305:2: error: Audio policy no longer supports legacy .conf configuration format
    #error Audio policy no longer supports legacy .conf configuration format

--- device.mk
+++ device.mk
@@ -43,6 +43,15 @@ PRODUCT_PACKAGES += \
     xaplay \
     enctune.conf
 
+# Audio policy configuration
+USE_XML_AUDIO_POLICY_CONF := 1
+PRODUCT_COPY_FILES += \
+	frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
+	frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
+	frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
+	frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
+	frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml
+
 # aptXHD
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/aptXHD/libaptX_encoder.so:$(TARGET_COPY_OUT_VENDOR)/lib/libaptX_encoder.so \
