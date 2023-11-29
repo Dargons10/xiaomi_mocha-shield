57d95e1bae9575c5f0169c5e528cb100ecacb19a
apple <105837597+380583067@users.noreply.github.com>
2023-11-29 20:43:55 +0800

    Add files via upload

--- system_prop.mk
+++ system_prop.mk
@@ -5,12 +5,17 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.bt.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac \
     persist.vendor.btstack.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldacs
 
+# BT
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.bt.bdaddr_path=/data/mocha_btmacaddr.txt
+
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
     drm.service.enabled=true
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.ui.hw=true \
     debug.sf.disable_backpressure=1 \
     debug.sf.latch_unsignaled=1
 
@@ -33,3 +38,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Wifi
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.debug.wfd.enable=1
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+	persist.sys.usb.config=mtp
+
+PRODUCT_PROPERTY_OVERRIDES += \
+  persist.service.adb.enable=1 \
+  persist.service.debuggable=1 \
+  persist.sys.usb.config=mtp,adb
